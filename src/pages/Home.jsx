e6bc303f3406f48216f08c0ce254e7d1f4b0d681
moisesfdasilva <msesfernandes@gmail.com>
2022-10-18 21:13:17 -0300

    Ajuste nas regiões em Home.jsx.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,19 +6,27 @@ class Home extends React.Component {
       <main>
         <h1>Ver Países Pelo Continente:</h1>
         <h2>Américas</h2>
-          <h3>América do Norte</h3>
-          <h3>América Central</h3>
-          <h3>América do Sul</h3>
+          <h4>América do Norte</h4>
+          <h4>América Latina e Caribe</h4>
         <h2>Europa</h2>
-          <h3>Europa Ocidental</h3>
-          <h3>Europa Oriental</h3>
+          <h4>Europa meridional (Sul da Europa)</h4>
+          <h4>Europa ocidental (Oeste da Europa)</h4>
+          <h4>Europa oriental (Leste da Europa)</h4>
+          <h4>Europa setentrional (Norte da Europa)</h4>
         <h2>África</h2>
-          <h3>África Subsaarina</h3>
-          <h3>África Central</h3>
+          <h4>África setentrional (Norte da África)</h4>
+          <h4>África subsaariana</h4>
         <h2>Ásia</h2>
-          <h3>Ocidental</h3>
-          <h3>Oriental</h3>
+          <h4>Ásia central</h4>
+          <h4>Ásia meridional (Sul da Ásia)</h4>
+          <h4>Ásia ocidental (Oeste da Ásia)</h4>
+          <h4>Ásia oriental (Leste da Ásia)</h4>
+          <h4>Sudeste da Ásia</h4>
         <h2>Oceânia</h2>
+          <h4>Austrália e Nova Zelândia</h4>
+          <h4>Melanésia</h4>
+          <h4>Micronésia</h4>
+          <h4>Polinésia</h4>
         <h2>Antartida</h2>
       </main>
     );
