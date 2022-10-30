ea9d3819594754cc7b9c65feedbb9b4b19bdb78d
moisesfdasilva <msesfernandes@gmail.com>
2022-10-30 16:39:51 -0300

    Construída a página Home, com o uso de context.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Home extends React.Component {
-  render() {
-    return (
-      <main>
-        <h1>Ver Países Pelo Continente:</h1>
-        <h2>Américas</h2>
-          <h4>América do Norte</h4>
-          <h4>América Latina e Caribe</h4>
-        <h2>Europa</h2>
-          <h4>Europa meridional (Sul da Europa)</h4>
-          <h4>Europa ocidental (Oeste da Europa)</h4>
-          <h4>Europa oriental (Leste da Europa)</h4>
-          <h4>Europa setentrional (Norte da Europa)</h4>
-        <h2>África</h2>
-          <h4>África setentrional (Norte da África)</h4>
-          <h4>África subsaariana</h4>
-        <h2>Ásia</h2>
-          <h4>Ásia central</h4>
-          <h4>Ásia meridional (Sul da Ásia)</h4>
-          <h4>Ásia ocidental (Oeste da Ásia)</h4>
-          <h4>Ásia oriental (Leste da Ásia)</h4>
-          <h4>Sudeste da Ásia</h4>
-        <h2>Oceânia</h2>
-          <h4>Austrália e Nova Zelândia</h4>
-          <h4>Melanésia</h4>
-          <h4>Micronésia</h4>
-          <h4>Polinésia</h4>
+function Home() {
+  const [continentsObj, setContinentsObj] = useState({
+    enableAmerica: false,
+    enableEurope: false,
+    enableAfrica: false,
+    enableAsia: false,
+    enableOceania: false,
+  });
+  const { enableAmerica, enableEurope, enableAfrica, enableAsia,
+    enableOceania } = continentsObj;
+
+  const selectContinent = (enable, binary) => {
+    setContinentsObj((state) => ({
+      ...state,
+      [enable]: !binary,
+    }));
+  };
+
+  return (
+    <main>
+        <h1>Continentes:</h1>
+        <h2
+          onClick={ () => selectContinent('enableAmerica', enableAmerica) }
+        >América</h2>
+          { (enableAmerica) && (
+            <section>
+              <h4>Norte</h4>
+              <h4>Latina e Caribe</h4>
+            </section>
+          )}
+        <h2
+          onClick={ () => selectContinent('enableEurope', enableEurope) }
+        >Europa</h2>
+          { (enableEurope) && (
+            <section>
+              <h4>Meridional (Sul da Europa)</h4>
+              <h4>Ocidental (Oeste da Europa)</h4>
+              <h4>Oriental (Leste da Europa)</h4>
+              <h4>Setentrional (Norte da Europa)</h4>
+            </section>
+          )}
+        <h2
+          onClick={ () => selectContinent('enableAfrica', enableAfrica) }
+        >África</h2>
+          { (enableAfrica) && (
+            <section>
+              <h4>Setentrional (Norte da África)</h4>
+              <h4>Subsaariana</h4>
+            </section>
+          )}
+        <h2
+          onClick={ () => selectContinent('enableAsia', enableAsia) }
+        >Ásia</h2>
+          { (enableAsia) && (
+            <section>
+              <h4>Central</h4>
+              <h4>Meridional (Sul da Ásia)</h4>
+              <h4>Ocidental (Oeste da Ásia)</h4>
+              <h4>Oriental (Leste da Ásia)</h4>
+              <h4>Sudeste</h4>
+            </section>
+          )}
+        <h2
+          onClick={ () => selectContinent('enableOceania', enableOceania) }
+        >Oceânia</h2>
+          { (enableOceania) && (
+            <section>
+              <h4>Austrália e Nova Zelândia</h4>
+              <h4>Melanésia</h4>
+              <h4>Micronésia</h4>
+              <h4>Polinésia</h4>
+            </section>
+          )}
         <h2>Antartida</h2>
       </main>
-    );
-  }
+  );
 }
 
 export default Home;
