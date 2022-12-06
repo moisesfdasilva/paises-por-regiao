0fe9a1fbda910eead42a0724a7869257074f2e62
moisesfdasilva <msesfernandes@gmail.com>
2022-12-05 22:29:47 -0300

    fix: corrigido o bug causado por existir "fonte" e "fontes", o qual não permitia a realização do map após dividir a string em um array.

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -7,19 +7,26 @@ import Footer from '../components/Footer';
 function Country() {
   useEffect(() => { window.scrollTo(0, 0); }, []);
   const { country } = useContext(CountriesContext);
-  const { id, nome, flag, linguas, governo, area, historico,
-    localizacao } = country;
+  const { id, nome, flag, linguas, governo, area, historico, localizacao } = country;
 
-  const aboutCountry = historico.split('Fontes:');
+  let aboutCountry = [];
+  if (historico.includes('Fontes:')) {
+    aboutCountry = historico.split('Fontes:');
+  } else if (historico.includes('Fonte:')) {
+    aboutCountry = historico.split('Fonte:');
+  } else {
+    aboutCountry = [historico, 'https://www.ibge.gov.br/']
+  }
+
   const references = (aboutCountry[1].trim()).split(`https://`);
   references.shift();
 
-  let textAjust = aboutCountry[0].replace('. ','--');
-  textAjust = textAjust.replace('. ','.__');
-  textAjust = textAjust.replace('--', '. ');
+  let textAjust = aboutCountry[0].replaceAll('. ','--');
+  textAjust = textAjust.replaceAll('.','.__');
+  textAjust = textAjust.replaceAll('--', '. ');
   const newTexts = textAjust.split('__');
-  
-  const formatNumber = new Intl.NumberFormat();
+
+  const formatNumber = new Intl.NumberFormat('pt-BR');
   return (
     <article>
       <Header />
@@ -28,60 +35,56 @@ function Country() {
           { `${nome.abreviado} (${id['ISO-3166-1-ALPHA-2']}/` }
           { `${id['ISO-3166-1-ALPHA-3']})` }
         </h1>
-        <img src={ flag } alt={ id['ISO-3166-1-ALPHA-3'] }/>
+        <img style={ { border:'solid 1px grey' } } src={ flag } alt={ id['ISO-3166-1-ALPHA-3'] }/>
         <div className="ui section divider"/>
         <h3 className="ui header">Nome:</h3>
-        <ul>
-          <li>{ `${nome.abreviado} (em Português, pt-BR); ` }</li>
-          <li>{ `${nome['abreviado-EN']} (em Inglês, en); e ` }</li>
-          <li>{ `${nome['abreviado-ES']}, (em Espanhol, es).` }</li>
-        </ul>
+        <div style={ { paddingLeft: '15px' } }>
+          <p>{ `${nome.abreviado} (em Português, pt-BR); ` }</p>
+          <p>{ `${nome['abreviado-EN']} (em Inglês, en); e ` }</p>
+          <p>{ `${nome['abreviado-ES']}, (em Espanhol, es).` }</p>
+        </div>
         <div className="ui section divider"/>
         <h3 className="ui header">Área:</h3>
-        <ul>
-          <li>{ `${formatNumber.format(area.total)}km².` }</li>
-        </ul>
+        <div style={ { paddingLeft: '15px' } }>
+          <p>{ `${formatNumber.format(area.total)}km².` }</p>
+        </div>
         <div className="ui section divider"/>
         <h3 className="ui header">Região:</h3>
-        <ul>
-          <li>
+        <div style={ { paddingLeft: '15px' } }>
+          <p>
             { `${localizacao['regiao'].nome} (` }
             { `${localizacao['sub-regiao'].nome}).` }
-          </li>
-        </ul>
+          </p>
+        </div>
         <div className="ui section divider"/>
         <h3 className="ui header">Capital:</h3>
-        <ul>
-          <li>{ `${governo.capital.nome}.` }</li>
-        </ul>
+        <div style={ { paddingLeft: '15px' } }>
+          <p>{ `${governo.capital.nome}.` }</p>
+        </div>
         <div className="ui section divider"/>
         <h3 className="ui header">Idioma:</h3>
-        <ul>
-          <li>{ `${linguas[0].nome}.` }</li>
-        </ul>
+        <div style={ { paddingLeft: '15px' } }>
+          <p>{ `${linguas[0].nome}.` }</p>
+        </div>
         <div className="ui section divider"/>
         <h3 className="ui header">Unidade monetária:</h3>
-        <ul>
-          <li>{ `${country['unidades-monetarias'][0].nome}.` }</li>
-        </ul>
+        <div style={ { paddingLeft: '15px' } }>
+          <p>{ `${country['unidades-monetarias'][0].nome}.` }</p>
+        </div>
         <div className="ui section divider"/>
         <h3 className="ui header">Sobre o país:</h3>
-        <ul>
+        <div style={ { paddingLeft: '15px' } }>
           { newTexts.map((newText, index) => (
-            <li key={ index }>
-              { newText }
-            </li>
+            <p key={ index }>{ newText }</p>
           )) }
-        </ul>
+        </div>
         <div className="ui section divider"/>
         <h3 className="ui header">Fonte(s):</h3>
-        <ul>
+        <div style={ { paddingLeft: '15px' } }>
           { references.map((reference, index) => (
-            <li key={ index }>
-              { `https:// ${reference}` }
-            </li>
+            <p key={ index }>{ `https://${reference}` }</p>
           )) }
-        </ul>
+        </div>
       </div>
       <Footer />
     </article>
